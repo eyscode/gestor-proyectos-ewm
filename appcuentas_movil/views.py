cb318896fc4db3887862ade302e49d992e54a2b1
Eysenck <eyscode@gmail.com>
2012-07-21 20:33:44 -0500

    view_login_movil

--- appcuentas_movil/views.py
+++ appcuentas_movil/views.py
@@ -4,19 +4,13 @@ from django.http import HttpResponse
 from django.utils import simplejson
 
 def view_login_movil(request):
-    to_json = None
+    to_json = -1
     if request.is_ajax() and request.method == "POST":
-        username = request.POST.get("username","")
-        password = request.POST.get("password","")
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
         if username and password:
-            user = authenticate(username=username,password=password)
+            user = authenticate(username=username, password=password)
             if user is not None:
-                login(request,user)
-                to_json={
-                    "id": request.user.id
-                }
-                HttpResponse(simplejson.dumps(to_json), mimetype='application/json')
-    to_json={
-        "id": -1
-    }
+                login(request, user)
+                to_json = request.user.id
     return HttpResponse(simplejson.dumps(to_json), mimetype='application/json')
